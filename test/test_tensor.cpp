251efe38433460f6f2389311175fd49b9ed80e6f
skystriker1997 <k77705@protonmail.com>
2023-10-03 17:15:28 +1100

    more tests for tensor

--- test/test_tensor.cpp
+++ test/test_tensor.cpp
@@ -5,7 +5,7 @@
 
 
 
-SCENARIO("tensor can be operated", "[tensor]") {
+SCENARIO("tensor can be operated properly", "[tensor]") {
 
     using namespace sky_infer;
 
@@ -14,11 +14,11 @@ SCENARIO("tensor can be operated", "[tensor]") {
         std::vector<float> data1 = {1, 1, 1, 1};
         std::vector<float> data2 = {1, 2, 3, 4};
 
-        Tensor<float> x(std::vector<int>{1, 2, 2}, data1, true);
-        Tensor<float> y(std::vector<int>{1, 2, 2}, data2, true);
+        Tensor<float> x(std::vector<int>{1, 2, 2}, data1);
+        Tensor<float> y(std::vector<int>{1, 2, 2}, data2);
 
-        REQUIRE(y.ReadMatrix(0)(1,0)==2);
-        REQUIRE(y.ReadMatrix(0)(0,1)==3);
+        REQUIRE(y.ReadMatrix(0)(1,0)==3);
+        REQUIRE(y.ReadMatrix(0)(0,1)==2);
         REQUIRE(y.Max()==4);
         REQUIRE(y.Min()==1);
 
@@ -26,10 +26,12 @@ SCENARIO("tensor can be operated", "[tensor]") {
             Tensor<float> z = x + y;
 
             THEN("z equals adding coefficients from x and y correspondingly") {
-                REQUIRE(z.ReadShape() == x.ReadShape());
+                REQUIRE(z.Channels() == x.Channels());
+                REQUIRE(z.Rows() == x.Rows());
+                REQUIRE(z.Cols() == x.Cols());
                 REQUIRE(z.ReadMatrix(0)(0,0)==2);
-                REQUIRE(z.ReadMatrix(0)(1,0)==3);
-                REQUIRE(z.ReadMatrix(0)(0,1)==4);
+                REQUIRE(z.ReadMatrix(0)(1,0)==4);
+                REQUIRE(z.ReadMatrix(0)(0,1)==3);
                 REQUIRE(z.ReadMatrix(0)(1,1)==5);
             }
         }
@@ -37,29 +39,46 @@ SCENARIO("tensor can be operated", "[tensor]") {
             Tensor<float> z = x * y;
 
             THEN("z follows the rule of matrix dot product") {
-                REQUIRE(z.ReadShape() == std::vector<int>{1,2,2});
-                REQUIRE(z.ReadMatrix(0)(0,0)==3);
-                REQUIRE(z.ReadMatrix(0)(1,0)==3);
-                REQUIRE(z.ReadMatrix(0)(0,1)==7);
-                REQUIRE(z.ReadMatrix(0)(1,1)==7);
+                REQUIRE(z.Channels() == x.Channels());
+                REQUIRE(z.Rows() == x.Rows());
+                REQUIRE(z.Cols() == x.Cols());
+                REQUIRE(z.ReadMatrix(0)(0,0)==4);
+                REQUIRE(z.ReadMatrix(0)(1,0)==4);
+                REQUIRE(z.ReadMatrix(0)(0,1)==6);
+                REQUIRE(z.ReadMatrix(0)(1,1)==6);
             }
         }
         WHEN("tensor z = x % y") {
             Tensor<float> z = x % y;
 
             THEN("z equals multiplying coefficients from x and y correspondingly") {
-                REQUIRE(z.ReadShape() == x.ReadShape());
+                REQUIRE(z.Channels() == x.Channels());
+                REQUIRE(z.Rows() == x.Rows());
+                REQUIRE(z.Cols() == x.Cols());
                 REQUIRE(z.ReadMatrix(0)(0,0)==1);
-                REQUIRE(z.ReadMatrix(0)(1,0)==2);
-                REQUIRE(z.ReadMatrix(0)(0,1)==3);
+                REQUIRE(z.ReadMatrix(0)(1,0)==3);
+                REQUIRE(z.ReadMatrix(0)(0,1)==2);
                 REQUIRE(z.ReadMatrix(0)(1,1)==4);
+            }
+        }
+        WHEN("x and y swap") {
+            x.Swap(y);
+
+            THEN("z equals multiplying coefficients from x and y correspondingly") {
+                REQUIRE(x.ReadMatrix(0)(0,0)==1);
+                REQUIRE(x.ReadMatrix(0)(1,0)==3);
+                REQUIRE(x.ReadMatrix(0)(0,1)==2);
+                REQUIRE(x.ReadMatrix(0)(1,1)==4);
+                x.Swap(y);
             }
         }
         WHEN("Tensor z is padding x with 0 at top, bottom, left and right by 1 respectively") {
             Tensor<float> z = x.Padding(std::vector<int>{1,1,1,1}, 0);
 
             THEN("z has been surrounded by 0") {
-                REQUIRE(z.ReadShape() == std::vector{1,4,4});
+                REQUIRE(z.Channels() == 1);
+                REQUIRE(z.Rows() == 4);
+                REQUIRE(z.Cols() == 4);
                 REQUIRE(z.ReadMatrix(0).block(1,1,2,2) == x.ReadMatrix(0));
                 REQUIRE(z.ReadMatrix(0)(0,0)==0);
                 REQUIRE(z.ReadMatrix(0)(0,3)==0);
@@ -67,6 +86,36 @@ SCENARIO("tensor can be operated", "[tensor]") {
                 REQUIRE(z.ReadMatrix(0)(3,3)==0);
             }
         }
+        WHEN("Tensor y has flattened each matrix") {
+            Tensor<float> z = y.Reshape({4});
+
+            THEN("z has one matrix with 1 row and 4 cols") {
+                REQUIRE(z.Channels() == 1);
+                REQUIRE(z.Rows() == 1);
+                REQUIRE(z.Cols() == 4);
+                REQUIRE(z.ReadMatrix(0)(0,0)==1);
+                REQUIRE(z.ReadMatrix(0)(0,1)==2);
+                REQUIRE(z.ReadMatrix(0)(0,2)==3);
+                REQUIRE(z.ReadMatrix(0)(0,3)==4);
+            }
+        }
+        WHEN("Tensor y has flattened each matrix inplace") {
+            y.ReshapeInplace({4});
+
+            THEN("y has one matrix with 1 row and 4 cols") {
+                REQUIRE(y.Channels() == 1);
+                REQUIRE(y.Rows() == 1);
+                REQUIRE(y.Cols() == 4);
+                REQUIRE(y.ReadMatrix(0)(0,0)==1);
+                REQUIRE(y.ReadMatrix(0)(0,1)==2);
+                REQUIRE(y.ReadMatrix(0)(0,2)==3);
+                REQUIRE(y.ReadMatrix(0)(0,3)==4);
+            }
+        }
+
+
+
+
     }
 }
 
