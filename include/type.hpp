e85cf03df7d723690333290fbf360b976bf23167
skystriker1997 <k77705@protonmail.com>
2023-10-29 01:19:57 +1100

    update types

--- include/type.hpp
+++ include/type.hpp
@@ -8,50 +8,19 @@
 namespace sky_infer {
 
 
-//    enum OpdDataType {
-//        OpdUnknown,
-//        OpdFloat,
-//    };
-//
-//    enum AttrDataType {
-//        AttrUnknown,
-//        AttrFloat,
-//    };
-
-
-//    enum ParamDataType {
-//        ParamDummy,
-//
-//        ParamBool,
-//
-//        ParamInt,
-//        ParamFloat,
-//        ParamString,
-//
-//        ParamIntArray,
-//        ParamFloatArray,
-//        ParamStringArray
-//    };
-
-
     enum LayerType {
         Dummy,
-        Input,
-        Output,
         ReLU,
         Expression,
         Linear,
         SoftMax,
         MaxPooling,
         Flatten,
-        Sigmoid
+        Sigmoid,
+        Conv,
+        DeConv
     };
 
-
-//    enum Tensor {
-//        Dummy,
-//        Mul
-//    };
 
 
 }
